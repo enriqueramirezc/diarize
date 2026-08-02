485f14982d6edcbd8cd1575169d32f25e0a8d556
Enrique Ramirez <enrique.ramirez@ucr.ac.cr>
2026-08-02 00:32:36 -0600

    multi channel csv conversion

--- src/CSVWrite.cpp
+++ src/CSVWrite.cpp
@@ -1,9 +1,9 @@
 #include "CSVWrite.hpp"
 
 int CSVWrite::write(const std::string& output, const std::vector<int16_t>& samples,
-                     uint32_t sampleRate, int numChannels) { // Mono for now
-    if (numChannels != 1) {
-        std::cerr << "CSVWrite::write only supports mono audio for now" << std::endl;
+                     uint32_t sampleRate, int numChannels) {
+    if (numChannels < 1) {
+        std::cerr << "CSVWrite::write invalid channel count: " << numChannels << std::endl;
         return 0;
     }
 
@@ -15,22 +15,41 @@ int CSVWrite::write(const std::string& output, const std::vector<int16_t>& sampl
         return 0;
     }
 
+    // samples are interleaved per frame (frame = one sample per channel)
+    const std::size_t numFrames = samples.size() / numChannels;
+
     // Write header
-    file << "Time,Min,Max,numSample\n";
+    file << "Time";
+    for (int ch = 0; ch < numChannels; ch++) {
+      file << ",Ch" << ch << "Min,Ch" << ch << "Max";
+    }
+    file << ",numSample\n";
 
-    const std::size_t N = 100; // samples per window
-    for (std::size_t i = 0; i < samples.size(); i += N) {
-      std::size_t end = std::min(i + N, samples.size());
+    const std::size_t N = 100; // frames per window
+    for (std::size_t i = 0; i < numFrames; i += N) {
+      std::size_t end = std::min(i + N, numFrames);
 
-      auto minVal = samples[i];
-      auto maxVal = samples[i];
-      for (std::size_t j = i + 1; j < end; j++) {
-        if (samples[j] < minVal) minVal = samples[j];
-        if (samples[j] > maxVal) maxVal = samples[j];
+      std::vector<int16_t> minVal(numChannels);
+      std::vector<int16_t> maxVal(numChannels);
+      for (int ch = 0; ch < numChannels; ch++) {
+        minVal[ch] = samples[i * numChannels + ch];
+        maxVal[ch] = samples[i * numChannels + ch];
       }
 
-     double time = static_cast<double>(i) / sampleRate;
-      file << time << "," << minVal << "," << maxVal << "," << i << "\n";
+      for (std::size_t j = i; j < end; j++) {
+        for (int ch = 0; ch < numChannels; ch++) {
+          int16_t sample = samples[j * numChannels + ch];
+          if (sample < minVal[ch]) minVal[ch] = sample;
+          if (sample > maxVal[ch]) maxVal[ch] = sample;
+        }
+      }
+
+      double time = static_cast<double>(i) / sampleRate;
+      file << time;
+      for (int ch = 0; ch < numChannels; ch++) {
+        file << "," << minVal[ch] << "," << maxVal[ch];
+      }
+      file << "," << i << "\n";
     }
 
     // Close the file
